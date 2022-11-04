53a125fa89c5b2101a58952254a3773982099a74
Margrét Johnson <home@myfluga.local>
2022-11-04 13:28:13 +0000

    manually fixing it cause git noob

--- OrderService/presentation/endpoints.py
+++ OrderService/presentation/endpoints.py
@@ -6,7 +6,7 @@ import requests
 from business.order_service import OrderService
 from infrastructure.container import Container
 from models.empty_model import EmptyModel
-from models.order import Order
+from models.order import Order, SavedOrder, ForwardOrder
 from exchange import Exchange
 
 router = APIRouter(prefix='/orders')
@@ -26,42 +26,65 @@ async def get_order(order_id: int, order_service: OrderService = Depends(Provide
 @router.post('/', status_code=status.HTTP_201_CREATED)
 @inject
 async def post_order(order: Order, order_service: OrderService = Depends(Provide[Container.order_service])):
-	# TODO: if merchant does not exist
+	# if merchant does not exist
 	merchant = get_merchant(order['merchant_id'])
 	if merchant is None:
 		raise HTTPException(404, 'Merchant does not exist.')
-	# TODO: if buyer does not exist
+
+	# if buyer does not exist
 	buyer = get_buyer(order['buyer_id'])
 	if buyer is None:
 		raise HTTPException(404, 'Buyer does not exist.')
-	# TODO: if product does not exist
+
+	# if product does not exist
 	inventory = get_inventory(order['product_id'])
 	if inventory is None:
 		raise HTTPException(404, 'Product does not exist.')
-	# TODO: if product does exist but is sold out
+
+	# if product does exist but is sold out
 	elif ( inventory['quantity'] == 0 ) or ( inventory['quantity'] - inventory['reserved'] == 0 ):
 		raise HTTPException(404, 'Product is sold out.')
-	# TODO: if product is not sold by merchant
+
+	# if product is not sold by merchant
 	if merchant['merchant_id'] != inventory['merchant_id']:
 		raise HTTPException(404, 'Product does not belong to merchant.')
-	# TODO: if merchant does not allow discounts and discount field is other than 0 or null
+
+	# if merchant does not allow discounts and discount field is other than 0 or null
 	if order['discount'] != 0 and merchant['allowsDiscount'] is False:
 		raise HTTPException(404, 'Merchant does not allow discount.')
 
-	# TODO: save with credit card info hidden with ****
-	inserted_order_id: int  = order_service.post_buyer(order)
+	order_to_save = SavedOrder(
+		        product_id=order['product_id'],
+                merchant_id=order['merchant_id'],
+                buyer_id=order['buyer_id'],
+                card_number=order['credit_card']['card_number'],
+                total_price=(1 - order['discount']) * inventory['price']
+	)
+
+	order_to_forward = ForwardOrder(
+		product_id = order['product_id'],
+		merchant_id = order['merchant_id'],
+		buyer_id = order['buyer_id'],
+		buyer = buyer,
+		credit_card = order['credit_card'],
+		total_price=(1 - order['discount']) * inventory['price']
+		)
+
+	# save with credit card info hidden with ****
+	inserted_order_id: int  = order_service.post_buyer(order_to_save)
 	# publish order
-	exchange.publish(order)
+	# exchange.publish(order_to_forward)
+	print(order_to_forward)
 	return {'message': f'Inserted order with ID {inserted_order_id}'}
 
 
 async def get_merchant(merchant_id: int):
-	return requests.get('localhost:8001/merchant/%s', merchant_id)
+	return requests.get('localhost:8000/merchant/%s', merchant_id)
 
 async def get_buyer(buyer_id: int):
-	return requests.get('localhost:8002/buyer/%s', buyer_id)
+	return requests.get('localhost:8000/buyer/%s', buyer_id)
 
 async def get_inventory(product_id: int):
-	return requests.get('localhost:8003/products/%s', product_id)
+	return requests.get('localhost:8000/products/%s', product_id)
 
 
