459c161d9b54884fa94519682e7c8a7fc7b6a1a8
Daníel Freyr Grétarsson <danielfgretars@gmail.com>
2022-11-08 20:13:06 +0000

    final final final touches

--- ApiService/presentation/endpoints.py
+++ ApiService/presentation/endpoints.py
@@ -3,42 +3,57 @@ from typing import Union
 from fastapi import APIRouter, HTTPException, status
 import requests
 
-# merchant : 8002
-# payment : 8003
-# order : 8004
-# api : 8005
-
+# order:    8000
+# merchant: 8001
+# buyer:    8002
+# products: 8003
 
 router = APIRouter(prefix='/api')
 
+
+# ORDER ENDPOINTS
+@router.get('/orders/{order_id}', status_code=status.HTTP_201_CREATED)
+async def get_order(order_id: int):
+    response = requests.get(f'http://order_service:8000/orders/{order_id}')
+    response_json = response.json()
+    if response.status_code == 404:
+        raise HTTPException(404, response_json['detail'])
+    return response_json
+
+
+@router.post('/orders', status_code=status.HTTP_201_CREATED)
+async def post_order(order: dict):
+    return requests.post(f'http://order_service:8000/orders/', json=order).json()
+
+
+# MERCHANT ENDPOINTS
+@router.get('/merchants/{merchant_id}', status_code=status.HTTP_200_OK)
+async def get_merchant(merchant_id: int):
+    response = requests.get(f'http://merchant_service:8001/merchants/{merchant_id}')
+    response_json = response.json()
+    if response.status_code == 404:
+        raise HTTPException(404, response_json['detail'])
+    return response_json
+
+
+@router.post('/merchants', status_code=status.HTTP_201_CREATED)
+async def post_merchant(merchant: dict) -> int:
+    return requests.post(f'http://merchant_service:8001/merchants/', json=merchant).json()
 
 
 # BUYER ENDPOINTS
 @router.get('/buyers/{buyer_id}', status_code=status.HTTP_200_OK)
 async def get_buyer(buyer_id: int) -> Union[dict, None]:
-    response = requests.get(f'http://buyer_service:8004/buyers/{buyer_id}')
+    response = requests.get(f'http://buyer_service:8002/buyers/{buyer_id}')
     response_json = response.json()
     if response.status_code == 404:
         raise HTTPException(404, response_json['detail'])
-    res_dict = {
-        "buyerId": response_json['buyer_id'],
-        "ssn": response_json['ssn'],
-        "name": response_json['name'],
-        "email": response_json['email'],
-        "phoneNumber": response_json['phone_number']
-    }
-    return res_dict
+    return response_json
 
 
 @router.post('/buyers/', status_code=status.HTTP_201_CREATED)
 async def post_buyer(buyer: dict) -> int:
-    req_dict = {
-        "name": buyer['name'],
-        "ssn": buyer['ssn'],
-        "email": buyer['email'],
-        "phone_number": buyer['phoneNumber']
-    }
-    return requests.post('http://buyer_service:8004/buyers/', json=req_dict).json()
+    return requests.post('http://buyer_service:8002/buyers/', json=buyer).json()
 
 
 # PRODUCT ENDPOINTS
@@ -49,89 +64,12 @@ async def get_products(product_id: int) -> Union[dict, None]:
     response_json = response.json()
     if response.status_code == 404:
         raise HTTPException(404, response_json['detail'])
-    res_dict = {
-        "productId": response_json["product_id"],
-        "merchantId": response_json["merchant_id"],
-        "productName": response_json["product_name"],
-        "price": response_json["price"],
-        "quantity": response_json["quantity"],
-        "reserved": response_json["reserved"]
-    }
-    return res_dict
+    return response_json
 
 
 @router.post('/products/', status_code=status.HTTP_201_CREATED)
 async def post_product(product: dict) -> int:
-    req_dict = {
-        "merchant_id": product["merchantId"],
-        "product_name": product["productName"],
-        "price": product["price"],
-        "quantity": product["quantity"],
-        "reserved": 0
-    }
-    return requests.post('http://inventory_service:8003/products/', json=req_dict).json()
+    return requests.post('http://inventory_service:8003/products/', json=product).json()
 
 
-# MERCHANT ENDPOINTS
-@router.get('/merchants/{merchant_id}', status_code=status.HTTP_200_OK)
-async def get_merchant(merchant_id: int):
-    response = requests.get(
-        f'http://merchant_service:8002/merchants/{merchant_id}')
-    response_json = response.json()
-    if response.status_code == 404:
-        raise HTTPException(404, response_json['detail'])
-    res_dict = {
-        "merchantId": response_json['merchant_id'],
-        "name": response_json['name'],
-        "ssn": response_json['ssn'],
-        "email": response_json['email'],
-        "phoneNumber": response_json['phone_number'],
-        "allowsDiscount": response_json['allows_discount']
-    }
-    return res_dict
 
-
-@router.post('/merchants', status_code=status.HTTP_201_CREATED)
-async def post_merchant(merchant: dict) -> int:
-    req_dict = {
-        "name": merchant['name'],
-        "ssn": merchant['ssn'],
-        "email": merchant['email'],
-        "phone_number": merchant['phoneNumber'],
-        "allows_discount": merchant['allowsDiscount']
-    }
-    return requests.post(f'http://merchant_service:8002/merchants/', json=req_dict).json()
-
-
-# ORDER ENDPOINTS
-@router.get('/orders/{order_id}', status_code=status.HTTP_201_CREATED)
-async def get_order(order_id: int):
-    response = requests.get(f'http://order_service:8000/orders/{order_id}')
-    response_json = response.json()
-    if response.status_code == 404:
-        raise HTTPException(404, response_json['detail'])
-    res_dict = {
-        "productId": response_json["product_id"],
-        "merchantId": response_json["merchant_id"],
-        "buyerId": response_json["buyer_id"],
-        "cardNumber": response_json["card_number"],
-        "totalPrice ": response_json["total_price"]
-    }
-    return res_dict
-
-
-@router.post('/orders', status_code=status.HTTP_201_CREATED)
-async def post_order(order: dict):
-    req_dict = {
-        "product_id": order["productId"],
-        "merchant_id": order["merchantId"],
-        "buyer_id": order["buyerId"],
-        "credit_card": {
-            "card_number": order["creditCard"]["cardNumber"],
-            "expiration_month": order["creditCard"]["expirationMonth"],
-            "expiration_year": order["creditCard"]["expirationYear"],
-            "cvc": order["creditCard"]["cvc"]
-        },
-        "discount": order["discount"]
-    }
-    return requests.post(f'http://order_service:8000/orders/', json=req_dict).json()
--- README.md
+++ README.md
@@ -9,3 +9,21 @@
 ## Start program
 1. Update `./EmailService/.env` to include your Google Email and app password.
 2. Run `docker compose up -d --scale email_service=2` from the root directory.
+
+### Request body
+Please make sure to use snake_case in the request body, as the database would not play nice with camelCase.
+For example:
+```json
+{
+    "product_id": 1,
+    "merchant_id": 1,
+    "buyer_id": 1,
+    "credit_card": {
+        "card_number": "4510164231949222",
+        "expiration_month": "7",
+        "expiration_year": "2024",
+        "cvc": "321"
+    },
+    "discount": 0.2
+}
+```
